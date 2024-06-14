d6caeea905730c3a178e0af7703722c4cc289b90
John Matthew <john@compunique.com>
2024-06-13 20:18:24 -0700

    add Whitenoise

--- stockprices/settings.py
+++ stockprices/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -149,3 +150,5 @@ LOGGING = {
         "level": "INFO",
     },
 }
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
